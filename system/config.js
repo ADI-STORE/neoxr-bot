23d1008d98fa2eddc704fb82bce0f63a0d08004c
Kanna <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-10-18 16:45:19 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,21 +1,21 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '6288980870067'
 // Owner name
-global.owner_name = 'Customer Service'
-// Maximum upload file size limit (Default : 50 MB)
-global.max_upload = 50
+global.owner_name = 'Swift Developer'
+// Maximum upload file size limit (Default : 99999 MB)
+global.max_upload = 99999
 // Delay for spamming protection (Default : 3 seconds)
 global.cooldown = 3
-// User Limitation (Default : 25)
-global.limit = 25
-// Time to be temporarily banned and others (Default : 30 minutes)
+// User Limitation (Default : 100)
+global.limit = 100
+// Time to be temporarily banned and others (Default : 500 minutes)
 global.timer = 1800000
 // Symbols that are excluded when adding a prefix (Don't change it)
 global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6282141544557@c.us'
+global.forwards = '6288980870067@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
@@ -24,7 +24,7 @@ global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Source version
 global.version = '2.2.1',
 // Footer text
-global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
+global.footer = 'Siesta - Multi Device'
 // Global status
 global.status = Object.freeze({
    wait: Func.texted('bold', 'Processed . . .'),
