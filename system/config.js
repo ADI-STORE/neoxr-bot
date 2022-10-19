c6a93f91a399f05ca21ed3f32be9c4458530635f
RARA-CHANS <115915177+ADI-STORE@users.noreply.github.com>
2022-10-19 19:47:49 +0800

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 // Owner number
-global.owner = '6288980870067'
+global.owner = '6287841959988'
 // Owner name
 global.owner_name = 'Swift Developer'
 // Maximum upload file size limit (Default : 99999 MB)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6288980870067@c.us'
+global.forwards = '6287841959988@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
